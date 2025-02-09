11ab6367cf891d8fc85f731eb121e1c88d79522c
Siggy Nazimowitz <thewotter@icloud.com>
2025-02-09 15:29:00 -0500

    Update files
    Embeds look much nicer now!

--- backend.py
+++ backend.py
@@ -1,13 +1,66 @@
 import httpx
-from webhook import feed2
+import json
+from webhook import feed2, quotefeed
+from datetime import datetime as dt, timezone
+from dateutil import parser
+import pytz
 
 if len(feed2) > 0:
     for feed_post in feed2:
-    response = httpx.post(
-        r"https://discord.com/api/webhooks/123/webhook_token",
-        json={
-            "content": f"New Bluesky post! {most_recent_post}",
-            }
-        )
+        post_time = parser.isoparse(feed_post.post.record.created_at)
+        est = pytz.timezone('US/Eastern')
+        post_time = post_time.astimezone(est)
+        time_string = "Posted on {:%B %d, %Y} at {:%H:%M}".format(post_time, post_time)
+        response = httpx.post(
+            r"webhook_token",
+            data={"payload_json": json.dumps({
+                "embeds": [{
+                    "author": {
+                        "name": "your-name",
+                        "url": "handle",
+                        "icon_url": "bsky-icon-url"
+                    },
+                    "title": "New Bluesky Post!",
+                    "url": feed_post.post.uri,
+                    "description": feed_post.post.record.text,
+                    "footer": {
+                        "text": time_string,
+                        "icon_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Bluesky_Logo.svg/1200px-Bluesky_Logo.svg.png"
+                        }
+                    }]
+                })}
+            )
         # raise an exception if the operation failed.
         response.raise_for_status()
+
+if len(quotefeed) > 0:
+    for feed_post in quotefeed:
+        post_time = parser.isoparse(quotefeed[feed_post].post.record.created_at)
+        est = pytz.timezone('US/Eastern')
+        post_time = post_time.astimezone(est)
+        time_string = "Posted on {:%B %d, %Y} at {:%H:%M}".format(post_time, post_time)
+        post_string = f"{quotefeed[feed_post].post.record.text}\n\nQuoting\n> {feed_post}"
+        response = httpx.post(
+            r"webhook_token",
+            data={"payload_json": json.dumps({
+                "embeds": [{
+                    "author": {
+                        "name": "your-name",
+                        "url": "hadnle",
+                        "icon_url": "bsky-icon-url"
+                    },
+                    "title": "New Bluesky Post!",
+                    "url": str(quotefeed[feed_post].post.uri),
+                    "description": post_string,
+                    "footer": {
+                        "text": time_string,
+                        "icon_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Bluesky_Logo.svg/1200px-Bluesky_Logo.svg.png"
+                        }
+                    }]
+                })}
+            )
+        # raise an exception if the operation failed.
+        response.raise_for_status()
+        
+
+
--- webhook.py
+++ webhook.py
@@ -8,11 +8,16 @@ client = Client()
 profile = client.login('handle', 'password')
 profile_feed = client.get_author_feed('handle', filter='posts_no_replies')
 feed2 = []
+quotefeed = {}
 for feed_view in profile_feed.feed:
+    post_url = re.sub(r'at://did:plc:uri/app.bsky.feed.post/', r'https://bsky.app/profile/did:plc:your-uri/post/', feed_view.post.uri)
+    post_url = re.sub(r'your-uri', r'handle', post_url)   
     post_time = parser.isoparse(feed_view.post.record.created_at)
     if 'your-uri' in feed_view.post.uri and max(post_time, current_time - timedelta(hours=1)) == post_time:
+            if feed_view.post.record.embed != None:
+                post_fetched = client.get_posts([feed_view.post.record.embed.record.uri])
+                quoted_text = post_fetched.posts[0].record.text
+                feed_view.post.uri = post_url
+                quotefeed.update({quoted_text : feed_view})
+    else:
         feed2.append(feed_view)
-        
-for feed_view in feed2:
-    feed_view.post.uri = re.sub(r'at://did:plc:your-uri/app.bsky.feed.post/', r'https://bsky.app/profile/did:plc:your-uri/post/', feed_view.post.uri)
-    feed_view.post.uri = re.sub(r'did:plc:cargf4lbxd6cg25d637gt2c3', r'handle.', feed_view.post.uri)
