ce037cce5a22e2bd07e0e3b980e8eaa50be708ca
Siggy Nazimowitz <thewotter@icloud.com>
2025-02-10 15:55:17 -0500

    Add image support for embeds (also fix a minor typo)

--- backend.py
+++ backend.py
@@ -46,7 +46,7 @@ if len(quotefeed) > 0:
                 "embeds": [{
                     "author": {
                         "name": "your-name",
-                        "url": "hadnle",
+                        "url": "handle",
                         "icon_url": "bsky-icon-url"
                     },
                     "title": "New Bluesky Post!",
@@ -60,6 +60,46 @@ if len(quotefeed) > 0:
                 })}
             )
         # raise an exception if the operation failed.
+        response.raise_for_status()
+        
+if len(imagefeed) > 0:
+    for feed_post in imagefeed:
+        post_time = parser.isoparse(feed_post.post.record.created_at)
+        est = pytz.timezone('US/Eastern')
+        post_time = post_time.astimezone(est)
+        time_string = "Posted on {:%B %d, %Y} at {:%H:%M}".format(post_time, post_time)
+        json_string = json.dumps({
+            "embeds": [{
+                "author": {
+                    "name": "your-name",
+                    "url": "handle",
+                    "icon_url": "bsky-icon-url"
+                },
+                "title": "New Bluesky Post!",
+                "url": feed_post.post.uri,
+                "description": feed_post.post.record.text,
+                "footer": {
+                    "text": time_string,
+                    "icon_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Bluesky_Logo.svg/1200px-Bluesky_Logo.svg.png"
+                    },
+                "image": {
+                    "url": feed_post.post.embed.images[0].fullsize
+                    }
+                }]
+            })
+        image_json = ""
+        for i in range(1, len(feed_post.post.embed.images)):
+            json_string2 = json.dumps({
+                "url": feed_post.post.uri,
+                "image": {
+                    "url": feed_post.post.embed.images[i].fullsize
+                    }
+                })
+            image_json = image_json + json_string2
+        json_merged = json_string[:-2] + "," + image_json + "]}"
+        response = httpx.post(
+        r"webhook_token",
+        data={"payload_json": json_merged})
         response.raise_for_status()
         
 
--- webhook.py
+++ webhook.py
@@ -9,15 +9,19 @@ profile = client.login('handle', 'password')
 profile_feed = client.get_author_feed('handle', filter='posts_no_replies')
 feed2 = []
 quotefeed = {}
+imagefeed = []
 for feed_view in profile_feed.feed:
     post_url = re.sub(r'at://did:plc:uri/app.bsky.feed.post/', r'https://bsky.app/profile/did:plc:your-uri/post/', feed_view.post.uri)
     post_url = re.sub(r'your-uri', r'handle', post_url)   
     post_time = parser.isoparse(feed_view.post.record.created_at)
     if 'your-uri' in feed_view.post.uri and max(post_time, current_time - timedelta(hours=1)) == post_time:
-            if feed_view.post.record.embed != None:
+            if hasattr(feed_view.post.record.embed, 'record'):
                 post_fetched = client.get_posts([feed_view.post.record.embed.record.uri])
                 quoted_text = post_fetched.posts[0].record.text
                 feed_view.post.uri = post_url
                 quotefeed.update({quoted_text : feed_view})
+            elif hasattr(feed_view.post.record.embed, 'images'):
+                feed_view.post.uri = post_url
+                imagefeed.append(feed_view)
     else:
         feed2.append(feed_view)
